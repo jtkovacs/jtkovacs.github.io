53c3f8f5537a90ba6e0dc129ea0f8dec9e1df2d8
jtk <jacobtkovacs@gmail.com>
2017-04-25 12:23:15 -0700

    Changes to information-architecture.html

--- add_TOC.py
+++ add_TOC.py
@@ -45,7 +45,8 @@ foname = foname.split('/')
 foname.insert(-1, "TOCS")
 foname = '/'.join(foname)
 fout = open(foname, "w")
-fout.write('<p id="path"><a href="../pkb.html">PKB index</a> &gt; https://jtkovacs.github.io/REFS/HTML/'+foname[25:-7]+'.html</p>') 
+url = 'https://jtkovacs.github.io/REFS/HTML/'+foname[25:-7]+'.html'
+fout.write('<p id="path"><a href="../pkb.html">PKB index</a> &gt; <a href="'+url+'>'+url+'</a></p>') 
 # fout.write('<span id="path">https://jtkovacs.github.io/REFS/HTML/'+foname[25:-7]+'.html</span>')
 fout.write('<table class="TOC"><tr><td>')
 for row in TOC:
